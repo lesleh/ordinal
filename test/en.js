83a322f073f77e609c9772f9f4c615abbdd0c067
Leslie Hoare <iam@lesleh.co.uk>
2015-01-19 02:37:16 +0000

    Reformat tests.

--- test/en.js
+++ test/en.js
@@ -3,49 +3,24 @@ var en = require("../lib/en");
 
 chai.should();
 
-describe('English', function(){
-  it('should return 1st for 1', function(){
-    en(1).should.equal('1st');
-  });
-  
-  it('should return 2nd for 2', function(){
-    en(2).should.equal('2nd');
-  });
-  
-  it('should return 3rd for 3', function(){
-    en(3).should.equal('3rd');
-  });
-  
-  it('should return 4th for 4', function(){
-    en(4).should.equal('4th');
-  });
-  
-  it('should return 11th for 11', function(){
-    en(11).should.equal('11th');
-  });
-  
-  it('should return 12th for 12', function(){
-    en(12).should.equal('12th');
-  });
-  
-  it('should return 21st for 21', function(){
-    en(21).should.equal('21st');
-  });
-  
-  it('should return 101st for 101', function(){
-    en(101).should.equal('101st');
-  });
-  
-  it('should return 111th for 111', function(){
-    en(111).should.equal('111th');
-  });
-  
-  it('should return 121st for 121', function(){
-    en(121).should.equal('121st');
-  });
-  
-  it('should return 1031st for 1031', function(){
-    en(1031).should.equal('1031st');
-  });
+var expected = {
+  1: 'st',
+  2: 'nd',
+  3: 'rd',
+  4: 'th',
+  11: 'th',
+  12: 'th',
+  21: 'st',
+  101: 'st',
+  111: 'th',
+  121: 'st',
+  1031: 'st'
+};
 
+describe('English', function(){
+  for(var key in expected) {
+    it('should return ' + expected[key] + ' for ' + key, function(){
+      en.suffix(key).should.equal(expected[key]);
+    });
+  }
 });
